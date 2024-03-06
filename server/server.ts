5bc2f92f913104b7c08484f0b7e21c21768ab668
dokker19 <dkyang1023@gmail.com>
2024-03-06 18:22:29 -0500

    get endpoint

--- server/server.ts
+++ server/server.ts
@@ -6,6 +6,10 @@ require('dotenv').config();
 const PORT=process.env.PORT;
 console.log(PORT)
 
+app.get('/', (req, res) => {
+    res.send('Hello World');
+})
+
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
